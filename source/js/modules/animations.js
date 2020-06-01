1f673cc438fda8732187eb1aec137d141150b89b
ekompanets <amelie.kom@gmail.com>
2020-06-02 01:33:09 +0300

    Правки линтера

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -6,6 +6,8 @@ export default () => {
   // подписка на окончание анимации на экране Правила
   document.querySelector(`.rules__item:last-child`).onanimationend = () => {
     document.querySelector(`.rules__link`).classList.add(`show-link`);
-    setTimeout(() => {document.querySelector(`.rules__link`).classList.remove(`show-link`);}, 1000);
-  }
+    setTimeout(() => {
+      document.querySelector(`.rules__link`).classList.remove(`show-link`);
+    }, 1000);
+  };
 };
