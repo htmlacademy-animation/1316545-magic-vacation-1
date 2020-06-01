02853d179f6e8f31991ea4f766d515865ed433b3
ekompanets <amelie.kom@gmail.com>
2020-06-02 01:26:08 +0300

    Добавила анимацию списка и кнопки правил

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -2,4 +2,10 @@ export default () => {
   document.addEventListener(`DOMContentLoaded`, () => {
     document.querySelector(`body`).classList.add(`load`);
   });
+
+  // подписка на окончание анимации на экране Правила
+  document.querySelector(`.rules__item:last-child`).onanimationend = () => {
+    document.querySelector(`.rules__link`).classList.add(`show-link`);
+    setTimeout(() => {document.querySelector(`.rules__link`).classList.remove(`show-link`);}, 1000);
+  }
 };
